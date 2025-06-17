eae402ac1231e4b1b555d299b752522c2c5d008d
chenguangzeng <chenguangzeng@cgz.com>
2025-06-18 01:16:08 +0800

    feat: add multicast

--- udp/multicast_client.cpp
+++ udp/multicast_client.cpp
@@ -18,6 +18,36 @@
  * 224.0.2.0～238.255.255.255 为用户可用的组播地址（临时组地址），全网范围内有效；
  * 239.0.0.0～239.255.255.255 为本地管理组播地址，仅在特定的本地范围内有效。
  */
+
+/**
+ * 端口：
+ * 
+ * 1.端口号的作用：
+ *   - 端口号用于区分同一主机上的不同应用程序或服务。例如ftp服务通常使用端口21，http服务通常使用端口80。
+ *   - 每个应用程序或服务在网络通信中都需要一个唯一的端口号，以便接收和发送数据。
+ * 
+ * 2.端口按照端口号分类：
+ *  - 知名端口（Well-Known Ports）：范围从0到1023，这些端口通常被系统保留给特定的服务和协议，如HTTP（80）、HTTPS（443）、FTP（21）等。
+ *  - 注册端口（Registered Ports）：范围从1024到49151，这些端口可以被用户或应用程序注册使用，但不如知名端口那么严格。
+ *  - 动态或者私有端口（Dynamic or Private Ports）：范围从49152到65535，这些端口通常用于临时的或私有的应用程序通信。
+ * 
+ * 3.端口按照协议分类：
+ *   - TCP端口：用于TCP协议的通信，提供可靠的、面向连接的服务。
+ *   - UDP端口：用于UDP协议的通信，提供无连接
+ *   - IP端口：用于IP协议的通信，通常用于多播和广播通信。
+ * 
+ * 4.端口占用：
+ *   - 某个端口被某个进程绑定(监听)，导致其他进程无法再以相同的协议(如UDP)和相同的IP地址绑定该端口。
+ *   - 端口相同，ip地址不同，可以绑定成功。
+ *   - 端口相同，协议不同，可以绑定成功。
+ *   - 端口相同，ip地址相同，协议相同，则无法绑定成功。
+ * 
+ * 5.端口复用：
+ *  - 端口复用是指允许多个套接字绑定到同一个端口上，以便在同一主机上运行多个服务或应用程序。
+ *  - 端口复用通常通过设置套接字选项：
+ *   - SO_REUSEADDR：允许多个套接字绑定到同一个端口上，但必须满足特定条件，如所有套接字都必须设置为SO_REUSEADDR选项或者绑定ip地址不同。
+ *   - SO_REUSEPORT：允许多个套接字绑定到相同ip和相同端口
+ */
  
 #define MAXLINE 4096
 #define PORT 9010
@@ -31,8 +61,24 @@ int main(void){
 
 	struct sockaddr_in multicast_address;
 	memset(&multicast_address, 0, sizeof(multicast_address));
+	multicast_address.sin_family = AF_INET;
+	multicast_address.sin_port = htons(PORT);
+	multicast_address.sin_addr.s_addr = inet_addr(multicast_addr_ctr); 
+
+	/**
+	 * @note socket需要绑定到多播端口，通常是INADDR_ANY跟多播端口。
+	 * @note 这样内核才会把发往改端口的数据包发送给你的socket
+	 * 
+	 * @note 尝试将ip地址修改成固定ip，发现收不到数据。
+	 * @note 这是绑定固定地址A，则只有发送给地址A的多播数据才会发送给你的socket。而多播包的目的地址是多播地址，不是你的IP地址。
+	 * @note 只有绑定到INADDR_ANY(0.0.0.0)，内核会将所有本机收到的，端口匹配的多播数据包发送给你的socket。
+	 */
 	struct sockaddr_in local_address;
 	memset(&local_address, 0, sizeof(local_address));
+	local_address.sin_family = AF_INET;
+	local_address.sin_port = htons(PORT);
+	local_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
 	socklen_t addr_len = sizeof(multicast_address);
 
 	/**
@@ -50,14 +96,13 @@ int main(void){
 		exit(0);
 	}
 
-	local_address.sin_family = AF_INET;
-	// 本地地址必须设置为INADDR_ANY，并且绑定到多播的端口
-	local_address.sin_port = htons(PORT);
-	local_address.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	// 设置端口复用
+	/**
+	 * @note 设置SO_REUSEADDR选项，允许端口复用，避免多个客户端从多播接受数据的时候发生端口占用
+	 * @note 如果只有一个客户端，改步骤就不需要了
+	 */
 	int opt = 1;
-	int ret = setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	int ret = -1;
+	ret = setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 	if(ret == -1){
 		printf("setsockopt error: %s(error: %d)\n", strerror(errno), errno);
 		close(client_fd);
@@ -71,9 +116,11 @@ int main(void){
 		exit(0);
 	}
 
-	// 加入多播组
-	mreq.imr_multiaddr.s_addr = inet_addr(multicast_addr_ctr);
-	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+	/**
+	 * @note 需要加入多播组，这样内核才会将发往指定多播组的数据转发给你的socket
+	 */
+	mreq.imr_multiaddr = multicast_address.sin_addr; // 多播组地址
+	mreq.imr_interface = local_address.sin_addr; // 本地地址
 	ret = setsockopt(client_fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
 	if(ret == -1){
 		printf("setsockopt IP_ADD_MEMBERSHIP error: %s(error: %d)\n", strerror(errno), errno);
--- udp/multicast_server.cpp
+++ udp/multicast_server.cpp
@@ -18,33 +18,29 @@ int main(void){
   const char* multicast_addr_ctr = "224.0.1.135";
   int ret = -1;
 
+  /**
+   * 多播发送数据一般创建socket，然后使用sendto函数发送数据到多播地址就行
+   */
 	server_fd = socket(AF_INET, SOCK_DGRAM, 0);
   if(server_fd == -1){
 		printf("create socket error: %s(error: %d)\n", strerror(errno), errno);
 		exit(0);
 	}
 
-  struct sockaddr_in servaddr;
-	memset(&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(PORT);
-	servaddr.sin_addr.s_addr = inet_addr(multicast_addr_ctr);
+  struct sockaddr_in multicast_addr;
+	memset(&multicast_addr, 0, sizeof(multicast_addr));
+	multicast_addr.sin_family = AF_INET;
+	multicast_addr.sin_port = htons(PORT);
+	multicast_addr.sin_addr.s_addr = inet_addr(multicast_addr_ctr);
 
   // 设置端口复用
-	int opt = 1;
-	ret = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-	if(ret == -1){
-		printf("setsockopt error: %s(error: %d)\n", strerror(errno), errno);
-		close(server_fd);
-		exit(0);
-	}
- 
-	ret = bind(server_fd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-  if(ret == -1){
-		printf("bind socket error: %s(error: %d)\n", strerror(errno), errno);
-    close(server_fd);
-		exit(0);
-	}
+	// int opt = 1;
+	// ret = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	// if(ret == -1){
+	// 	printf("setsockopt error: %s(error: %d)\n", strerror(errno), errno);
+	// 	close(server_fd);
+	// 	exit(0);
+	// }
 
   while(1){
     sleep(1); // Sleep for 1 second before sending the next message
@@ -54,7 +50,7 @@ int main(void){
     sendbuf[3] = 'l';
     sendbuf[4] = 'o';
     sendbuf[5] = '\0';
-    int len = sendto(server_fd, sendbuf, sizeof(sendbuf), 0, (struct sockaddr*)&servaddr, sizeof(servaddr));
+    int len = sendto(server_fd, sendbuf, sizeof(sendbuf), 0, (struct sockaddr*)&multicast_addr, sizeof(multicast_addr));
     if(len < 0){
       if(errno == EINTR){
         printf("recv interrupted, retrying...\n");
